02d7b8188eab51a84d36da656f7eb8fa79f3a44f
Vitor Wagner <vitormwagner@gmail.com>
2023-04-28 15:36:03 -0300

    Refactor: :recycle: Refactored login controller and service functions

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,16 +1,9 @@
 const LoginService = require('../services/login.services');
-const { createToken } = require('../utils/jwt.utils');
 
 async function login(req, res) {
   const { email, password } = req.body;
 
-  const user = await LoginService.login(email, password);
-
-  if (!user) {
-    return res.status(400).json({ message: 'Invalid fields' });
-  }
-
-  const token = createToken(user);
+  const token = await LoginService.login(email, password);
 
   return res.status(200).json({ token });
 }
--- src/services/login.services.js
+++ src/services/login.services.js
@@ -1,13 +1,17 @@
 const { User } = require('../models');
+const GenerateError = require('../utils/generateError');
+const { createToken } = require('../utils/jwt.utils');
 
 async function login(email, password) {
   const user = await User.findOne({ where: { email } });
 
   if (!user || password !== user.password) {
-    return null;
+    throw GenerateError(400, 'Invalid fields');
   }
 
-  return user;
+  const token = createToken(user);
+
+  return token;
 }
 
 module.exports = {
